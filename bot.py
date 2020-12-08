027fcf7ff8af37bf96789edcef1b3fff77548d74
omironenko <ol.mironenko@gmail.com>
2020-12-09 00:21:37 +0300

    initial

---
+++ bot.py
@@ -0,0 +1,147 @@
+import calendar
+
+import telebot
+
+import config
+from dbworker import reset_user, get_param, set_param, get_user_info
+from dfworker import get_tmpr, get_prcp, get_final, get_results
+
+
+bot = telebot.TeleBot(config.token)
+
+
+@bot.message_handler(commands=["help"])
+def user_reset(message):
+    uid = message.chat.id
+    bot.send_message(uid, 'Hello! I am a WhereIsWeatherBot.\n\
+        I can search for places in the world with particular weather conditions\n\
+        at particular week of the year.\
+        You can write /start to begin a dialog.\n\
+        Then I will ask you to send me the number of month and a day you are interested in.\n\
+        After that you should specify the range of mean daily temperature (in Celcius) and\n\
+        range of days with any precipitation per week.\n\
+        Eventually, you will get an image of the world map with all places found according\n\
+        to your criteria and links to Google map for at least five of them.\n\
+        You can send /start at any time to start over again.')
+
+@bot.message_handler(commands=["start"])
+def user_start(message):
+    uid = message.chat.id
+    reset_user(uid)
+    bot.send_message(uid, 'Give me the number of month, please [1-12]')
+    set_param(uid, 'state', config.States.S_MONTH.value)
+
+@bot.message_handler(func=lambda message: get_param(message.chat.id, 'state') == config.States.S_MONTH.value)
+def user_entering_day(message):
+    uid = message.chat.id
+    response = message.text
+    if response.isdigit() and 1 <= int(response) <= 12:
+        set_param(uid, 'month', response)
+        set_param(uid, 'last_day_in_month', calendar.monthrange(2020, get_param(uid, 'month'))[1])
+        bot.send_message(uid, "Give me the day, please [1-%s]" % get_param(uid, 'last_day_in_month'))
+        set_param(uid, 'state', config.States.S_DAY.value)
+    else:
+        bot.send_message(uid, 'Something went wrong. Send me the correct number of month, please [1-12]')
+
+@bot.message_handler(func=lambda message: get_param(message.chat.id, 'state') == config.States.S_DAY.value)
+def user_entering_tmpr_min(message):
+    uid = message.chat.id
+    response = message.text
+    if response.isdigit() and 1 <= int(response) <= get_param(uid, 'last_day_in_month'):
+        set_param(uid, 'day', response)
+        df_filtered_week, tmpr_min, tmpr_max = get_tmpr(uid, config.df_file, get_param(uid, 'month'), get_param(uid, 'day'))
+        set_param(uid, 'df_filtered_week', df_filtered_week)
+        set_param(uid, 'tmpr_min', tmpr_min)
+        set_param(uid, 'tmpr_max', tmpr_max)
+        bot.send_message(uid, "What is the minimum daily mean temperature you are looking for (in \u00b0C)? Actual range in the world for time you've chosen is: [{}-{}]".format(get_param(uid, 'tmpr_min'), get_param(uid, 'tmpr_max')))
+        set_param(uid, 'state', config.States.S_MIN_TEMP.value)
+    else:
+        bot.send_message(uid, "Something went wrong. Send me the correct day, please [1-%s]" % get_param(uid, 'last_day_in_month'))
+
+@bot.message_handler(func=lambda message: get_param(message.chat.id, 'state') == config.States.S_MIN_TEMP.value)
+def user_entering_tmpr_max(message):
+    uid = message.chat.id
+    response = message.text
+    good_response = False
+    try:
+        int(response)
+        good_response = True
+    except ValueError:
+        pass
+    if good_response and get_param(uid, 'tmpr_min') <= int(response) <= get_param(uid, 'tmpr_max'):
+        set_param(uid, 'tmpr_min_desired', response)
+        bot.send_message(uid, "What is the maximum daily mean temperature you are looking for (in \u00b0C)? Available range is: [{}-{}]".format(get_param(uid, 'tmpr_min_desired'), get_param(uid, 'tmpr_max')))
+        set_param(uid, 'state', config.States.S_MAX_TEMP.value)
+    else:
+        bot.send_message(uid, "Something went wrong. Send me the correct minimum temperature (in \u00b0C), please [{}-{}]".format(get_param(uid, 'tmpr_min'), get_param(uid, 'tmpr_max')))
+
+@bot.message_handler(func=lambda message: get_param(message.chat.id, 'state') == config.States.S_MAX_TEMP.value)
+def user_entering_prcp_min(message):
+    uid = message.chat.id
+    response = message.text
+    good_response = False
+    try:
+        int(response)
+        good_response = True
+    except ValueError:
+        pass
+    if good_response and get_param(uid, 'tmpr_min_desired') <= int(response) <= get_param(uid, 'tmpr_max'):
+        set_param(uid, 'tmpr_max_desired', response)
+        df_filtered_tmpr, prcp_min, prcp_max = get_prcp(uid, get_param(uid, 'df_filtered_week'), get_param(uid, 'tmpr_min_desired'), get_param(uid, 'tmpr_max_desired'))
+        set_param(uid, 'df_filtered_tmpr', df_filtered_tmpr)
+        set_param(uid, 'prcp_min', prcp_min)
+        set_param(uid, 'prcp_max', prcp_max)
+        bot.send_message(uid, "What is the minimum number of days with any kind of precipitation per week you prefer? By precipitation I mean any of the following: rain, snow, hail or thunder. Actual range of the number of such days per week in the world for chosen time is [{}-{}]".format(get_param(uid, 'prcp_min'), get_param(uid, 'prcp_max')))
+        set_param(uid, 'state', config.States.S_MIN_PREC_DAYS.value)
+    else:
+        bot.send_message(uid, "Something went wrong. Send me the correct maximum temperature (in \u00b0C), please [{}-{}]".format(get_param(uid, 'tmpr_min_desired'), get_param(uid, 'tmpr_max')))
+
+@bot.message_handler(func=lambda message: get_param(message.chat.id, 'state') == config.States.S_MIN_PREC_DAYS.value)
+def user_entering_prcp_max(message):
+    uid = message.chat.id
+    response = message.text
+    if response.isdigit() and get_param(uid, 'prcp_min') <= int(response) <= get_param(uid, 'prcp_max'):
+        set_param(uid, 'prcp_min_desired', response)
+        bot.send_message(uid, "What is the maximum number of days with any kind of precipitation per week that is OK for you? [{}-{}]".format(get_param(uid, 'prcp_min_desired'), get_param(uid, 'prcp_max')))
+        set_param(uid, 'state', config.States.S_MAX_PREC_DAYS.value)
+    else:
+        bot.send_message(uid, "Something went wrong. Send me the correct minimum number of days with precipitation per week, please [{}-{}]".format(get_param(uid, 'prcp_min'), get_param(uid, 'prcp_max')))
+
+@bot.message_handler(func=lambda message: get_param(message.chat.id, 'state') == config.States.S_MAX_PREC_DAYS.value)
+def user_entering_prcp(message):
+    uid = message.chat.id
+    response = message.text
+    if response.isdigit() and get_param(uid, 'prcp_min_desired') <= int(response) <= get_param(uid, 'prcp_max'):
+        set_param(uid, 'prcp_max_desired', response)
+        set_param(
+            uid,
+            'df_filtered_final',
+            get_final(
+                uid,
+                get_param(uid, 'df_filtered_tmpr'),
+                get_param(uid, 'prcp_min_desired'),
+                get_param(uid, 'prcp_max_desired')
+            )
+        )
+        map_file, record_count, urls = get_results(uid, get_param(uid, 'df_filtered_final'))
+        set_param(uid, 'map', map_file)
+        set_param(uid, 'record_count', record_count)
+        set_param(uid, 'urls', urls)
+
+        bot.send_message(uid, "I found %s unique places in the world with parameters of your choice.\nHere are some of them:" % get_param(uid, 'record_count'))
+        bot.send_message(uid, '\n'.join(eval(get_param(uid, 'urls'))))
+
+        bot.send_message(uid, "I also have a map with all records for you:")
+        bot.send_photo(uid, open(get_param(uid, 'map'), 'rb'), caption='Map')
+
+        bot.send_message(uid, "Good luck! Have a nice trip!")
+
+        set_param(uid, 'state', config.States.S_START.value)
+        reset_user(uid)
+        #bot.send_message(uid, "Debug info:" + get_user_info(uid))
+    else:
+        bot.send_message(uid, "Something went wrong. Send me the correct maximum number of days with precipitation per week, please [{}-{}]".format(get_param(uid, 'prcp_min_desired'), get_param(uid, 'prcp_max')))
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
---
+++ dfworker.py
@@ -0,0 +1,114 @@
+import datetime
+import random
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import pandas
+from mpl_toolkits.basemap import Basemap
+
+
+def get_tmpr(uid, df_file, month, day):
+    '''
+    По заданному номеру месяца и дню определяет номер недели в году для этого дня, 
+    начиная с 1 января, и из заданного файла с данными о погоде возвращает 
+    отфильтрованные в нём данные для этой недели в году.
+    '''
+    df = pandas.read_csv(df_file)
+
+    date_diff = datetime.date(2020, month, day) - datetime.date(2020, 1, 1)
+    week = date_diff.days // 7 + 1
+    
+    filtered_df = df[df['WEEK'] == week]
+    tmpr_min = filtered_df['TEMP'].min()
+    tmpr_max = filtered_df['TEMP'].max()
+
+    df_filtered_week = '%s-df_filtered_week.csv' % str(uid)
+    filtered_df.to_csv(df_filtered_week, index=False, encoding='utf-8')
+
+    return df_filtered_week, tmpr_min, tmpr_max
+
+def get_prcp(uid, df_file, desired_min_temp, desired_max_temp):
+    '''
+    По заданному диапазону желаемой температуры отфильтровывает из базы подходящие
+    наблюдения.
+    '''
+    df = pandas.read_csv(df_file)
+
+    filtered_df = df[(df['TEMP'] >= desired_min_temp) & (df['TEMP'] <= desired_max_temp)]
+    prcp_min = filtered_df['ANY_PRCP'].min()
+    prcp_max = filtered_df['ANY_PRCP'].max()
+
+    df_filtered_tmpr = '%s-df_filtered_tmpr.csv' % str(uid)
+    filtered_df.to_csv(df_filtered_tmpr, index=False, encoding='utf-8')
+
+    return df_filtered_tmpr, prcp_min, prcp_max
+
+def get_final(uid, df_file, prcp_min_desired, prcp_max_desired):
+    '''
+    По заданному диапазону желаемого количества дней с осадками отфильтровывает
+    из базы подходящие наблюдения.
+    '''
+    df = pandas.read_csv(df_file)
+
+    filtered_df = df[(df['ANY_PRCP'] >= prcp_min_desired) & (df['ANY_PRCP'] <= prcp_max_desired)]
+
+    df_filtered_final = '%s-df_filtered_final.csv' % str(uid)
+    filtered_df.to_csv(df_filtered_final, index=False, encoding='utf-8')
+
+    return df_filtered_final
+
+def get_results(uid, df_file):
+    '''
+    Получает на вход pandas DataFrame, наносит на карту точки по координатам (колонки 'LATITUDE, LONGITUDE'). Если диапазон между
+    максимальной и минимальной температурой (колонка TEMP) больше 1 градуса Цельсия, раскрашивает точки в зависимости от температуры.
+    Сохраняет файл с картой. На выходе - тапл с названием файла, количеством найденных уникальных
+    мест, лист со ссылками на google maps с <=5 из них.
+    '''
+    df = pandas.read_csv(df_file)
+
+    lngs = df['LONGITUDE'].tolist()
+    lats = df['LATITUDE'].tolist()
+    temps = df['TEMP'].tolist()
+    countries = df['COUNTRY'].unique().tolist()
+    
+    if len(lngs) > 100:
+        set_size = 10
+        set_alpha = 0.5
+    else:
+        set_size = 15
+        set_alpha = 0.6
+
+    if df['TEMP'].max() - df['TEMP'].min() > 1:
+        if df['TEMP'].min() < 0:
+            set_cmap = 'viridis'
+        else:
+            set_cmap = 'plasma'
+            
+    fig, ax = plt.subplots(figsize=(16, 8))
+    m = Basemap(ax=ax, llcrnrlon=-170, llcrnrlat=-60, urcrnrlon=190, urcrnrlat=85)
+    m.drawcoastlines(color='#333333', linewidth = 0.3)
+    m.drawcountries(linewidth = 0.3, linestyle = "--")
+    m.drawmapboundary(fill_color='lightcyan')
+    m.fillcontinents(color='silver', lake_color='lightcyan')
+    if df['TEMP'].max() - df['TEMP'].min() > 1:
+        dots = ax.scatter(lngs, lats, cmap=set_cmap, c=temps, s=set_size, alpha=set_alpha, zorder=10)
+        plt.colorbar(dots, orientation='horizontal', pad=0.01, aspect=50, ticks=list(range(df['TEMP'].min(), df['TEMP'].max()+1)), 
+                 format='%d\u00b0C')
+    else:
+        ax.scatter(lngs, lats, c='red', s=set_size, alpha=set_alpha, zorder=10)
+        
+    plt.savefig(str(uid) + '-map.png', bbox_inches='tight', pad_inches=0)
+    
+    if len(countries) > 5:
+        selected_countries = random.sample(countries, 5)
+    else:
+        selected_countries = countries
+    
+    df_selected = df[df['COUNTRY'].isin(selected_countries)]
+    
+    selected_places = df_selected.groupby('COUNTRY').agg(np.random.choice)
+    
+    urls = selected_places[['LATITUDE','LONGITUDE']].apply(lambda x: 'https://maps.google.com/?q=' + str(x['LATITUDE']) + ',' + str(x['LONGITUDE']) + '&ll=' + str(x['LATITUDE']) + ',' + str(x['LONGITUDE']) +'&z=10', axis=1).tolist()
+        
+    return (str(uid) + '-map.png', len(df['PLACE'].unique().tolist()), urls)
